da50a705848f30ffe3ebe0610cfb60fa9c86b657
Lior Gerstein <liorg@netcraft.local>
2016-05-09 10:54:03 +0300

    testing 123

---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+let App = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <h1>Hello World</h1>
+            </div>
+
+        );
+    }
+});
+
+export default App;
